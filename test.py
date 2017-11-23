91abec99c2ba2996e098238e13e35b022e3e61ba
Wouter Bulten <wouterbulten@mai-ai.org>
2017-11-23 11:08:52 +0100

    Update test script

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from keras import layers, models
 from keras.utils import to_categorical
+from keras.models import load_model
 import numpy as np
 
 input = layers.Input(shape=(10,))
@@ -10,16 +11,29 @@ model = models.Model(inputs=input, outputs=output)
 
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy'],
+              return_model_output=True)
 
 
-x = np.zeros((10, 10))
-x[0:5, :] = 1
-y = np.zeros(10)
-y[0:5] = 1
+x = np.zeros((100, 10))
+x[0:50, :] = 1
+y = np.zeros(100)
+y[0:50] = 1
 
 y = to_categorical(y)
-print(model.train_on_batch(x, y, return_model_output=True))
-print(model.test_on_batch(x, y, return_model_output=True))
+
+# Try normal functions
+print(model.train_on_batch(x, y))
+print(model.test_on_batch(x, y))
+
+# Now try fitting
+model.fit(x, y, batch_size=2, verbose=True)
+
 
 model.save('D:/test.h5')
+del model
+
+# Load model
+model2 = load_model('D:/test.h5')
+print(model2.train_on_batch(x, y))
+print(model2.test_on_batch(x, y))
